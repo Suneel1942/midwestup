7337f232eb0c6c0f4a1af1ea6d9f7881c78af58f
Hasibullah <hasibullah.mahmood@ruul.io>
2025-10-06 20:12:35 +0300

    Refactor Header and MenuDropdown components for improved readability and functionality; integrate classnames for dynamic styling and adjust layout styles in SCSS.

--- src/components/menu-dropdown.js
+++ src/components/menu-dropdown.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Link, navigate } from "gatsby";
-import { motion, useSpring, AnimatePresence } from "framer-motion";
-import { useOnClickOutside } from "@utils/useOnClickOutside";
-import { useWindowSize } from "@utils/useWindowSize";
-
-import * as styles from "@styles/dropdown.module.scss";
+import React, { useState, useRef, useEffect } from "react"
+import { Link, navigate } from "gatsby"
+import { motion, useSpring, AnimatePresence } from "framer-motion"
+import { useOnClickOutside } from "@utils/useOnClickOutside"
+import { useWindowSize } from "@utils/useWindowSize"
+import cln from "classnames"
+import * as styles from "@styles/dropdown.module.scss"
 
 const DropdownContainer = ({ isMobile, open, transitionType, children }) => {
   if (isMobile) {
@@ -27,7 +27,7 @@ const DropdownContainer = ({ isMobile, open, transitionType, children }) => {
       animate={{
         opacity: 1,
         y: 0,
-        boxShadow: open ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "0px 4px 10px rgba(0, 0, 0, 0.1)"
+        boxShadow: open ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "0px 4px 10px rgba(0, 0, 0, 0.1)",
       }}
       exit={{ opacity: 0, y: 10 }}
       style={{ translateX: "-50%" }}
@@ -37,7 +37,10 @@ const DropdownContainer = ({ isMobile, open, transitionType, children }) => {
       <div className={styles.spacing} />
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, boxShadow: open ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "0px 4px 10px rgba(0, 0, 0, 0.1)" }}
+        animate={{
+          opacity: 1,
+          boxShadow: open ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "0px 4px 10px rgba(0, 0, 0, 0.1)",
+        }}
         className={styles.arrow}
       />
       <ul>{children}</ul>
@@ -45,11 +48,11 @@ const DropdownContainer = ({ isMobile, open, transitionType, children }) => {
   )
 }
 
-export const MenuDropdown = ({ title, items, path }) => {
-  const [open, setOpen] = useState(false);
-  const [titleHovered, setTitleHovered] = useState(false);
-  const [curIndex, setCurIndex] = useState(-1);
-  const dropdownContainer = useRef(null);
+export const MenuDropdown = ({ title, items, path, className }) => {
+  const [open, setOpen] = useState(false)
+  const [titleHovered, setTitleHovered] = useState(false)
+  const [curIndex, setCurIndex] = useState(-1)
+  const dropdownContainer = useRef(null)
 
   const { width } = useWindowSize()
   const [isMobile, setMobile] = useState(width < 1181)
@@ -59,17 +62,17 @@ export const MenuDropdown = ({ title, items, path }) => {
       setMobile(width < 1181)
     }
     resize()
-    window.addEventListener('resize', resize)
+    window.addEventListener("resize", resize)
     return () => {
-      window.removeEventListener('resize', resize)
+      window.removeEventListener("resize", resize)
     }
-  },[width])
+  }, [width])
 
   useOnClickOutside(dropdownContainer, () => {
     setOpen(false)
-  });
+  })
 
-  const spring = useSpring(0);
+  const spring = useSpring(0)
 
   const options = items.map((item, index) => {
     function handleLink(e) {
@@ -81,9 +84,7 @@ export const MenuDropdown = ({ title, items, path }) => {
       <motion.li
         key={`option-${index + 1}`}
         animate={
-          curIndex === index
-            ? { backgroundColor: "#f0f0f0" }
-            : { backgroundColor: "#ffffff" }
+          curIndex === index ? { backgroundColor: "#f0f0f0" } : { backgroundColor: "#ffffff" }
         }
         whileTap={{ backgroundColor: "#f0f0f0" }}
         onHoverStart={() => setCurIndex(index)}
@@ -94,8 +95,8 @@ export const MenuDropdown = ({ title, items, path }) => {
         {item.title}
         {isMobile ? <a onClick={handleLink} /> : <Link to={item.link} />}
       </motion.li>
-    );
-  });
+    )
+  })
 
   return (
     <div
@@ -103,7 +104,7 @@ export const MenuDropdown = ({ title, items, path }) => {
       onClick={() => setOpen(!open)}
       onMouseOver={() => setOpen(true)}
       onMouseLeave={() => setOpen(false)}
-      className={styles.menuDropdownContainer}
+      className={cln(styles.menuDropdownContainer, className)}
     >
       <div
         type="button"
@@ -113,9 +114,24 @@ export const MenuDropdown = ({ title, items, path }) => {
         style={{ "--menu-item-line": path ? "underline" : "none" }}
       >
         <p>{title}</p>
-        {isMobile && <motion.svg xmlns="http://www.w3.org/2000/svg" width="14" height="10" viewBox="0 0 14 10" fill="none" animate={open ? { rotateZ: 180 } : { rotateZ: 0 }}>
-          <path d="M1.5 1.5L7 8.5L12.5 1.5" stroke="black" strokeWidth="1.5" strokeLinecap="square" strokeLinejoin="round"/>
-        </motion.svg>}
+        {isMobile && (
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="10"
+            viewBox="0 0 14 10"
+            fill="none"
+            animate={open ? { rotateZ: 180 } : { rotateZ: 0 }}
+          >
+            <path
+              d="M1.5 1.5L7 8.5L12.5 1.5"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeLinecap="square"
+              strokeLinejoin="round"
+            />
+          </motion.svg>
+        )}
       </div>
       <AnimatePresence>
         {open && (
@@ -125,5 +141,5 @@ export const MenuDropdown = ({ title, items, path }) => {
         )}
       </AnimatePresence>
     </div>
-  );
-};
+  )
+}
