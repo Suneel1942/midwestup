7c5387bb6c6cc67d400f4e49171c3d3ba63eb8d8
Hasibullah <hasibullah.mahmood@ruul.io>
2025-10-08 21:38:19 +0300

    Refactor Carousel and Image components to improve layout and styling; adjust image handling for better responsiveness and quality.

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,16 +3,20 @@ import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export const Image = ({ src, alt, ...props }) => {
-  const { allFile: { nodes: images }} = useStaticQuery(graphql`
+  const {
+    allFile: { nodes: images },
+  } = useStaticQuery(graphql`
     query {
-      allFile (filter: { extension: { regex: "/(jpg)|(png)|(jpeg)|(webp)/" }}) {
+      allFile(filter: { extension: { regex: "/(jpg)|(png)|(jpeg)|(webp)/" } }) {
         nodes {
           relativePath
           childImageSharp {
             gatsbyImageData(
               layout: FULL_WIDTH
               placeholder: BLURRED
-              formats: [AUTO,WEBP]
+              formats: [AUTO, WEBP]
+              quality: 100
+              breakpoints: [750, 1080, 1366, 1920]
             )
           }
         }
@@ -25,5 +29,17 @@ export const Image = ({ src, alt, ...props }) => {
   // console.log('imageimageimageimage', image)
   const imageData = getImage(image)
 
-  return <GatsbyImage image={imageData} alt={alt} {...props} />
+  return (
+    <GatsbyImage
+      image={imageData}
+      alt={alt}
+      {...props}
+      imgStyle={{
+        objectFit: "cover",
+        objectPosition: "center center",
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  )
 }
